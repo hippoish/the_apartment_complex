132f9b75f6cf131eb94b57be2633befd5be50957
Taylor Britton <k.taylor.britton@gmail.com>
2016-07-12 15:42:14 -0700

    finished part 4

--- apartment.rb
+++ apartment.rb
@@ -1,10 +1,15 @@
 class Apartment
-  attr_reader :unit, :num_beds, :num_baths
+  attr_reader :unit, :num_beds, :num_baths, :price
   attr_accessor :tenants
 
   def initialize unit, num_beds, num_baths
     @unit = unit
     @num_beds = num_beds
     @num_baths = num_baths
+  end
+
+  def price
+    price = num_beds * 1000 + num_baths * 500
+    @price = "$#{price} per month"
   end
 end
